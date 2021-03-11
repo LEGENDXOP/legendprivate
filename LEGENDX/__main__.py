111498a6d6dff20f889b0fa05ee1e94c75b4d82b
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-11 14:54:13 +0530

    Hmm

---
+++ LEGENDX/__main__.py
@@ -0,0 +1,15 @@
+from sys import argv, exit
+from LEGENDX import tbot
+from LEGENDX import TOKEN
+import LEGENDX.events
+
+try:
+    tbot.start(bot_token=TOKEN)
+except Exception:
+    print("Network Error !")
+    exit(1)
+
+if len(argv) not in (1, 3, 4):
+    tbot.disconnect()
+else:
+    tbot.run_until_disconnected()
